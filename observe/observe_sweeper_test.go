4241f3363fe742db9aca99a26e711ac35a381684
João Taveira Araújo <joao@observeinc.com>
2022-04-14 12:19:37 -0700

    ci: add datastream, poller sweepers
    Go through all the datastreams and pollers and delete.
    
    Change-Id: Ifd964596f9cafb35a2e4523896536ae4f6fa308c

--- observe/observe_sweeper_test.go
+++ observe/observe_sweeper_test.go
@@ -21,6 +21,14 @@ func init() {
 		Name: "observe_monitor_sweep",
 		F:    monitorSweeperFunc,
 	})
+	resource.AddTestSweepers("observe_poller_sweep", &resource.Sweeper{
+		Name: "observe_poller_sweep",
+		F:    pollerSweeperFunc,
+	})
+	resource.AddTestSweepers("observe_datastream_sweep", &resource.Sweeper{
+		Name: "observe_datastream_sweep",
+		F:    datastreamSweeperFunc,
+	})
 }
 
 func sharedClient(s string) (*observe.Client, error) {
@@ -110,6 +118,99 @@ func monitorSweeperFunc(s string) error {
 			if prefixRe.MatchString(name) {
 				log.Printf("[WARN] Deleting %s [id=%s]\n", name, id)
 				if err := client.DeleteMonitor(ctx, id); err != nil {
+					return err
+				}
+			}
+		}
+	}
+	return nil
+}
+
+func pollerSweeperFunc(s string) error {
+	client, err := sharedClient(s)
+	if err != nil {
+		return err
+	}
+
+	ctx := context.Background()
+
+	workspaces, err := client.ListWorkspaces(ctx)
+	if err != nil {
+		return err
+	}
+
+	for _, workspace := range workspaces {
+		result, err := client.Meta.Run(ctx, `
+		query pollers($workspaceId: ObjectId!) {
+			pollers(workspaceId: $workspaceId) {
+				id
+				config {
+					name
+				}
+			}
+		}`, map[string]interface{}{
+			"workspaceId": workspace.ID,
+		})
+
+		if err != nil {
+			return fmt.Errorf("failed to lookup pollers: %w", err)
+		}
+
+		for _, i := range result["pollers"].([]interface{}) {
+			var (
+				item   = i.(map[string]interface{})
+				id     = item["id"].(string)
+				config = item["config"].(map[string]interface{})
+			)
+			name := config["name"].(string)
+			if prefixRe.MatchString(name) {
+				log.Printf("[WARN] Deleting %s [id=%s]\n", name, id)
+				if err := client.DeletePoller(ctx, id); err != nil {
+					return err
+				}
+			}
+		}
+	}
+	return nil
+}
+
+func datastreamSweeperFunc(s string) error {
+	client, err := sharedClient(s)
+	if err != nil {
+		return err
+	}
+
+	ctx := context.Background()
+
+	workspaces, err := client.ListWorkspaces(ctx)
+	if err != nil {
+		return err
+	}
+
+	for _, workspace := range workspaces {
+		result, err := client.Meta.Run(ctx, `
+		query datastreams($workspaceId: ObjectId!) {
+			datastreams(workspaceId: $workspaceId) {
+				id
+				name
+			}
+		}`, map[string]interface{}{
+			"workspaceId": workspace.ID,
+		})
+
+		if err != nil {
+			return fmt.Errorf("failed to lookup datastreams: %w", err)
+		}
+
+		for _, i := range result["datastreams"].([]interface{}) {
+			var (
+				item = i.(map[string]interface{})
+				id   = item["id"].(string)
+				name = item["name"].(string)
+			)
+			if prefixRe.MatchString(name) {
+				log.Printf("[WARN] Deleting %s [id=%s]\n", name, id)
+				if err := client.DeleteDatastream(ctx, id); err != nil {
 					return err
 				}
 			}
