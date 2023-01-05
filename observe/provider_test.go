2f7eafff9eea6ceb7a42ffdcc48ccb91632bcea1
Ben Drucker <ben.drucker@observeinc.com>
2023-01-05 15:50:39 -0800

    test: remove custom terraform installer
    The acceptance test framework will automatically install Terraform if needed. This functionality re-implemented that while attempting to cache the result.
    
    Jenkins runs `make testacc` in Docker. Every test run executes in a new container, meaning the cache will never be hit.
    
    I've removed `TF_ACC_TERRAFORM_VERSION` entirely, which will cause acceptance tests to use the latest version. If we had a specific reason to pin a version the test framework will handle the environment variable. But by default, I'd think we should test on the latest version.
    
    Reverts https://gerrit.observeinc.com/c/terraform-provider-observe/+/19869
    
    Change-Id: I0394aec3e2cdfc6dfad31226b1e9c9f13af5efb2

--- observe/provider_test.go
+++ observe/provider_test.go
@@ -1,14 +1,9 @@
 package observe
 
 import (
-	"context"
-	"log"
 	"os"
 	"testing"
 
-	"github.com/hashicorp/go-version"
-	"github.com/hashicorp/hc-install/product"
-	"github.com/hashicorp/hc-install/releases"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
@@ -19,40 +14,6 @@ func init() {
 	testAccProvider = Provider()
 	testAccProviders = map[string]*schema.Provider{
 		"observe": testAccProvider,
-	}
-
-	if cacheDir := os.Getenv("TF_ACC_TERRAFORM_CACHE_DIR"); cacheDir != "" {
-		cacheTerraformBinary(cacheDir)
-	}
-}
-
-func cacheTerraformBinary(cacheDir string) {
-	var (
-		tfPath    = os.Getenv("TF_ACC_TERRAFORM_PATH")
-		tfVersion = os.Getenv("TF_ACC_TERRAFORM_VERSION")
-	)
-
-	// do not cache if an exact path was given
-	if tfPath != "" {
-		return
-	}
-
-	installer := &releases.ExactVersion{
-		Product:    product.Terraform,
-		InstallDir: cacheDir,
-		Version:    version.Must(version.NewVersion(tfVersion)),
-	}
-
-	execPath, err := installer.Install(context.Background())
-	if err != nil {
-		log.Printf("[WARN] failed to cache terraform binary: %s", err)
-		return
-	}
-
-	log.Printf("[DEBUG] downloaded version %s to %s", tfVersion, execPath)
-
-	if err := os.Setenv("TF_ACC_TERRAFORM_PATH", execPath); err != nil {
-		log.Println("[WARN] failed to set TF_ACC_TERRAFORM_PATH")
 	}
 }
 
