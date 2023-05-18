e84bcab729646c126263309649b45ae4d32fd0e2
Colin Hutchinson <colin.hutchinson@observeinc.com>
2023-05-18 16:20:14 -0400

    OB-14629: re-enable workspace tests
    Change-Id: I239d8964850b5764725ec0e288b11fbcdb783fe1

--- observe/resource_workspace_test.go
+++ observe/resource_workspace_test.go
@@ -1,44 +1,48 @@
 package observe
 
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
 // Verify we can create workspace
-//func TestAccObserveWorkspaceCreate(t *testing.T) {
-//	randomPrefix := acctest.RandomWithPrefix("tf")
-//
-//	resource.ParallelTest(t, resource.TestCase{
-//		PreCheck:  func() { testAccPreCheck(t) },
-//		Providers: testAccProviders,
-//		Steps: []resource.TestStep{
-//			{
-//				Config: fmt.Sprintf(configPreamble+`
-//				resource "observe_workspace" "first" {
-//					name 	  = "%s"
-//				}`, randomPrefix),
-//				Check: resource.ComposeTestCheckFunc(
-//					resource.TestCheckResourceAttr("observe_workspace.first", "name", randomPrefix),
-//				),
-//			},
-//			{
-//				Config: fmt.Sprintf(configPreamble+`
-//				resource "observe_workspace" "first" {
-//					name 	  = "%s-renamed"
-//				}
-//
-//				resource "observe_dataset" "first" {
-//					workspace = observe_workspace.first.oid
-//					name      = "Test"
-//
-//					inputs = {
-//						"observation" = data.observe_dataset.observation.oid
-//					}
-//
-//					stage {}
-//				}
-//				`, randomPrefix),
-//				Check: resource.ComposeTestCheckFunc(
-//					resource.TestCheckResourceAttr("observe_workspace.first", "name", randomPrefix+"-renamed"),
-//					resource.TestCheckResourceAttr("observe_dataset.first", "name", "Test"),
-//				),
-//			},
-//		},
-//	})
-//}
+// Only running this test in CI.
+// Requires the additional_workspaces customer config to be set to allow
+func TestAccObserveWorkspaceCreate(t *testing.T) {
+	randomPrefix := acctest.RandomWithPrefix("tf")
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+			if os.Getenv("CI") != "true" {
+				t.Skip("Skipping test: CI environment variable is not set to 'true'")
+			}
+		},
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: fmt.Sprintf(configPreamble+`
+				resource "observe_workspace" "first" {
+					name 	  = "%s"
+				}`, randomPrefix),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("observe_workspace.first", "name", randomPrefix),
+				),
+			},
+			{
+				Config: fmt.Sprintf(configPreamble+`
+				resource "observe_workspace" "first" {
+					name 	  = "%s-renamed"
+				}
+				`, randomPrefix),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("observe_workspace.first", "name", randomPrefix+"-renamed"),
+				),
+			},
+		},
+	})
+}
