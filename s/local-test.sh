7032dfcc1bbd13f22108ba841f9d18f9fdfa456b
Ben Drucker <bvdrucker@gmail.com>
2023-07-27 14:11:08 -0700

    Delete `s/` (#20)

--- s/local-test.sh
+++
@@ -1,6 +0,0 @@
-export OBSERVE_CUSTOMER="$(~/observe/s/testbox get-current-customer)"
-export OBSERVE_DOMAIN=observe-sandbox.com:3443
-export OBSERVE_USER_EMAIL=sandbox@observeinc.com
-export OBSERVE_USER_PASSWORD="very very secret"
-export OBSERVE_INSECURE=true
-make testacc
