3345dd2b27cd3a93615d0776203113b74e6cfbe2
Ben Drucker <bvdrucker@gmail.com>
2023-12-11 15:19:07 -0800

    docs: make provider schema deterministic

--- main.go
+++ main.go
@@ -5,7 +5,10 @@ import (
 	"github.com/observeinc/terraform-provider-observe/observe"
 )
 
-//go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs generate
+//go:generate env -u OBSERVE_CUSTOMER go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs generate
+// `env -u` unsets a variable.  This prevents the go generate command from inheriting it.
+// When set, `OBSERVE_CUSTOMER` defines the default for the `customer` provider attribute.
+// This makes this required field optional, since a default is set.
 
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
