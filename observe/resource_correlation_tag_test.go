2f4e4f6040cff55b9354b3a79fe53421074aab69
Nitish Chinta <nitish.chinta@observeinc.com>
2024-05-10 22:42:30 +0000

    Use a unique name for tag names and datastream names in tests

--- observe/resource_correlation_tag_test.go
+++ observe/resource_correlation_tag_test.go
@@ -1,23 +1,27 @@
 package observe
 
 import (
+	"fmt"
 	"testing"
 
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
 
 func TestCorrelationTagCreation(t *testing.T) {
+	randomPrefix := acctest.RandomWithPrefix("tf")
+
 	resource.Test(t, resource.TestCase{
 		PreCheck:  func() { testAccPreCheck(t) },
 		Providers: testAccProviders,
 		Steps: []resource.TestStep{
 			{
-				Config: linkConfigPreamble + `
+				Config: fmt.Sprintf(linkConfigPreamble+`
 					resource "observe_correlation_tag" "example" {
-					name = "key.name"
+					name = "%[1]s-key.name"
 					dataset = observe_dataset.a.oid
 					column = "key"
-				}`,
+				}`, randomPrefix),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr("observe_correlation_tag.example", "name", "key.name"),
 					resource.TestCheckResourceAttr("observe_correlation_tag.example", "column", "key"),
@@ -26,28 +30,28 @@ func TestCorrelationTagCreation(t *testing.T) {
 			},
 			// Using the same config, there should not be any diff.
 			{
-				Config: linkConfigPreamble + `
+				Config: fmt.Sprintf(linkConfigPreamble+`
 					resource "observe_correlation_tag" "example" {
-					name = "key.name"
+					name = "%[1]s-key.name"
 					dataset = observe_dataset.a.oid
 					column = "key"
-				}`,
+				}`, randomPrefix),
 				PlanOnly:           true,
 				ExpectNonEmptyPlan: false,
 			},
 			{
 				// Making any change to the config should delete and recreate the tag (in-place update is not supported)
-				Config: linkConfigPreamble + `
+				Config: fmt.Sprintf(linkConfigPreamble+`
 					resource "observe_correlation_tag" "example" {
-					name = "key.name-2"
+					name = "%[1]s-key.name-2"
 					dataset = observe_dataset.a.oid
 					column = "key"
-				}`,
+				}`, randomPrefix),
 				Check: resource.TestCheckResourceAttr("observe_correlation_tag.example", "name", "key.name-2"),
 			},
 			{
 				// Removing the config should delete the tag
-				Config: linkConfigPreamble,
+				Config: fmt.Sprintf(linkConfigPreamble, randomPrefix),
 				Check:  resource.TestCheckNoResourceAttr("observe_correlation_tag.example", "name"),
 			},
 		},
