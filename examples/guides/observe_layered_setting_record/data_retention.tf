4ca9a5f820d365f6f49a06c044a8a381fff9503d
vikram <vikram@observeinc.com>
2024-07-01 17:13:54 +0000

    fix tf formatting

--- examples/guides/observe_layered_setting_record/data_retention.tf
+++ examples/guides/observe_layered_setting_record/data_retention.tf
@@ -1,9 +1,9 @@
 # layered setting for a datastream retention for a specific stream
 # targets a datastream OID
 resource "observe_layered_setting_record" "datastream_retention_my_first" {
-    workspace     = data.observe_workspace.default.oid
-    name          = "Layered Setting For Retention 30 days my first datastream"
-    setting       = "DataRetention.periodDays"
-    value_int64 = 30
-    target        = resource.observe_datastream.my_first_datastream.oid
+  workspace   = data.observe_workspace.default.oid
+  name        = "Layered Setting For Retention 30 days my first datastream"
+  setting     = "DataRetention.periodDays"
+  value_int64 = 30
+  target      = resource.observe_datastream.my_first_datastream.oid
 }
--- examples/guides/observe_layered_setting_record/query_gov.tf
+++ examples/guides/observe_layered_setting_record/query_gov.tf
@@ -8,11 +8,11 @@ data "observe_oid" "customer" {
 # Query Governor - Customer Level Throttled
 # Target must be a customer OID
 resource "observe_layered_setting_record" "base_tenant_credit_limit_throttled" {
-    workspace     = data.observe_workspace.default.oid
-    name          = "New Global Credit Limit THROTTLED"
-    setting       = "QueryGovernor.throttledLimitCreditsPerDay"
-    value_float64 = 100.0
-    target        = data.observe_oid.customer.oid
+  workspace     = data.observe_workspace.default.oid
+  name          = "New Global Credit Limit THROTTLED"
+  setting       = "QueryGovernor.throttledLimitCreditsPerDay"
+  value_float64 = 100.0
+  target        = data.observe_oid.customer.oid
 }
 
 # Applying hard and soft limits for all users
@@ -22,11 +22,11 @@ resource "observe_layered_setting_record" "base_tenant_credit_limit_throttled" {
 # users inherit this limit, unless they are targeted specifically
 # as a user OID
 resource "observe_layered_setting_record" "all_users_credit_limit_soft" {
-    workspace     = data.observe_workspace.default.oid
-    name          = "All Users Query Limit THROTTLED"
-    setting       = "QueryGovernor.userThrottledLimitCreditsPerDay"
-    value_float64 = 50.0
-    target        = data.observe_oid.customer.oid
+  workspace     = data.observe_workspace.default.oid
+  name          = "All Users Query Limit THROTTLED"
+  setting       = "QueryGovernor.userThrottledLimitCreditsPerDay"
+  value_float64 = 50.0
+  target        = data.observe_oid.customer.oid
 }
 
 # Query Governor - User Level - All Users - Hard
@@ -35,11 +35,11 @@ resource "observe_layered_setting_record" "all_users_credit_limit_soft" {
 # users inherit this limit, unless they are targeted specifically
 # as a user OID
 resource "observe_layered_setting_record" "all_users_credit_limit_hard" {
-    workspace     = data.observe_workspace.default.oid
-    name          = "All Users Query Limit HARD"
-    setting       = "QueryGovernor.userCreditsPerDay"
-    value_float64 = 80.0
-    target        = data.observe_oid.customer.oid
+  workspace     = data.observe_workspace.default.oid
+  name          = "All Users Query Limit HARD"
+  setting       = "QueryGovernor.userCreditsPerDay"
+  value_float64 = 80.0
+  target        = data.observe_oid.customer.oid
 }
 
 
@@ -53,44 +53,44 @@ data "observe_user" "carl_chumplin" {
 
 # Query Governor - User Level - User 1 - Throttled
 resource "observe_layered_setting_record" "base_admin_credit_limit" {
-    workspace     = data.observe_workspace.default.oid
-    name          = "User 1 Query Limit THROTTLED"
-    setting       = "QueryGovernor.userThrottledLimitCreditsPerDay"
-    value_float64 = 5.0
-    target        = data.observe_user.kyle_champlin.oid
+  workspace     = data.observe_workspace.default.oid
+  name          = "User 1 Query Limit THROTTLED"
+  setting       = "QueryGovernor.userThrottledLimitCreditsPerDay"
+  value_float64 = 5.0
+  target        = data.observe_user.kyle_champlin.oid
 }
 
 # Query Governor - User Level - User 1 - Hard
 resource "observe_layered_setting_record" "base_admin_credit_limit_hard" {
-    workspace     = data.observe_workspace.default.oid
-    name          = "User 1 Query Limit HARD"
-    setting       = "QueryGovernor.userCreditsPerDay"
-    value_float64 = 10.0
-    target        = data.observe_user.kyle_champlin.oid
+  workspace     = data.observe_workspace.default.oid
+  name          = "User 1 Query Limit HARD"
+  setting       = "QueryGovernor.userCreditsPerDay"
+  value_float64 = 10.0
+  target        = data.observe_user.kyle_champlin.oid
 }
 
 # User 2 Lookup
 data "observe_user" "carl_credit" {
-    email = "carlCreditLimits@observeinc.com"
+  email = "carlCreditLimits@observeinc.com"
 }
 
 
 resource "observe_layered_setting_record" "base_admin_credit_limit_throttled" {
-    workspace     = data.observe_workspace.default.oid
-    name          = "User 2 Query Limit throttled"
-    setting       = "QueryGovernor.userThrottledLimitCreditsPerDay"
-    value_float64 = 10.0
-    target        = data.observe_user.carl_credit.oid
+  workspace     = data.observe_workspace.default.oid
+  name          = "User 2 Query Limit throttled"
+  setting       = "QueryGovernor.userThrottledLimitCreditsPerDay"
+  value_float64 = 10.0
+  target        = data.observe_user.carl_credit.oid
 }
 
 
 # Query Governor - User Level - User 2 - Throttled
 resource "observe_layered_setting_record" "base_reader_credit_limit" {
-    workspace     = data.observe_workspace.default.oid
-    name          = "User 2 Credit Limit HARD"
-    setting       = "QueryGovernor.userCreditsPerDay"
-    value_float64 = 20.0
-    target        = data.observe_user.carl_credit.oid
+  workspace     = data.observe_workspace.default.oid
+  name          = "User 2 Credit Limit HARD"
+  setting       = "QueryGovernor.userCreditsPerDay"
+  value_float64 = 20.0
+  target        = data.observe_user.carl_credit.oid
 }
 
 # There are also global limits, that are evaluated last
@@ -100,17 +100,17 @@ resource "observe_layered_setting_record" "base_reader_credit_limit" {
 # Target must be a customer OID
 
 resource "observe_layered_setting_record" "base_tenant_credit_limit_transforms" {
-    workspace     = data.observe_workspace.default.oid
-    name          = "New Global Credit Limit HARD Transforms"
-    setting       = "TransformGovernor.creditsPerDay"
-    value_float64 = 200.0
-    target        = data.observe_oid.customer.oid
+  workspace     = data.observe_workspace.default.oid
+  name          = "New Global Credit Limit HARD Transforms"
+  setting       = "TransformGovernor.creditsPerDay"
+  value_float64 = 200.0
+  target        = data.observe_oid.customer.oid
 }
 
 resource "observe_layered_setting_record" "base_tenant_credit_limit_query" {
-    workspace     = data.observe_workspace.default.oid
-    name          = "New Global Credit Limit HARD query"
-    setting       = "QueryGovernor.creditsPerDay"
-    value_float64 = 200.0
-    target        = data.observe_oid.customer.oid
+  workspace     = data.observe_workspace.default.oid
+  name          = "New Global Credit Limit HARD query"
+  setting       = "QueryGovernor.creditsPerDay"
+  value_float64 = 200.0
+  target        = data.observe_oid.customer.oid
 }
