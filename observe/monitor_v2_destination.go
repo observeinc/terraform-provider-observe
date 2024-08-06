b470388a6da3b2f01312b063e4d0b7e05ea14baa
Owen Goebel <owen.goebel@observeinc.com>
2024-08-06 22:34:06 +0000

    oops

--- observe/monitor_v2_destination.go
+++ observe/monitor_v2_destination.go
@@ -19,13 +19,18 @@ func newMonitorV2DestinationInput(actInput *gql.MonitorV2ActionInput) (input *gq
 	}
 
 	if actInput.Email != nil {
-		input.Email.Users = make([]types.UserIdScalar, 0)
-		for _, usr := range actInput.Email.Users {
-			input.Email.Users = append(input.Email.Users, usr)
+		input.Email = &gql.MonitorV2EmailDestinationInput{}
+		if len(input.Email.Users) > 0 {
+			input.Email.Users = make([]types.UserIdScalar, 0)
+			for _, usr := range actInput.Email.Users {
+				input.Email.Users = append(input.Email.Users, usr)
+			}
 		}
-		input.Email.Addresses = make([]string, 0)
-		for _, addr := range actInput.Email.Addresses {
-			input.Email.Addresses = append(input.Email.Addresses, addr)
+		if len(input.Email.Addresses) > 0 {
+			input.Email.Addresses = make([]string, 0)
+			for _, addr := range actInput.Email.Addresses {
+				input.Email.Addresses = append(input.Email.Addresses, addr)
+			}
 		}
 	}
 
