2b591787d3b465f81e7ca343fb7a42c56a5768dc
Owen Goebel <owen.goebel@observeinc.com>
2024-08-08 16:14:44 +0000

    address vikram's comments

--- observe/resource_monitor_v2_action_test.go
+++ observe/resource_monitor_v2_action_test.go
@@ -69,13 +69,8 @@ func TestAccObserveMonitorV2ActionEmail(t *testing.T) {
 							fragments = jsonencode({
 								foo = "bar"
 							})
-						}
-						destination {
-							email {
-								addresses = ["test@observeinc.com"]
-								users = [data.observe_user.system.oid]
-							}
-							description = "an interesting dest description"
+							addresses = ["test@observeinc.com"]
+							users = [data.observe_user.system.oid]
 						}
 						name = "%[1]s"
 						description = "an interesting description"
@@ -90,9 +85,8 @@ func TestAccObserveMonitorV2ActionEmail(t *testing.T) {
 					resource.TestCheckResourceAttr("observe_monitor_v2_action.act", "email.0.fragments", "{\"foo\":\"bar\"}"),
 					resource.TestCheckResourceAttr("observe_monitor_v2_action.act", "email.0.subject", "somebody once told me"),
 					resource.TestCheckResourceAttr("observe_monitor_v2_action.act", "email.0.body", "the world is gonna roll me"),
-					resource.TestCheckResourceAttr("observe_monitor_v2_action.act", "destination.0.email.0.addresses.0", "test@observeinc.com"),
-					resource.TestCheckResourceAttr("observe_monitor_v2_action.act", "destination.0.description", "an interesting dest description"),
-					resource.TestCheckResourceAttr("observe_monitor_v2_action.act", "destination.0.email.0.users.#", "1"),
+					resource.TestCheckResourceAttr("observe_monitor_v2_action.act", "email.0.addresses.0", "test@observeinc.com"),
+					resource.TestCheckResourceAttr("observe_monitor_v2_action.act", "email.0.users.#", "1"),
 				),
 			},
 		},
