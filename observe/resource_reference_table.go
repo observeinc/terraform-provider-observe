a2b9d28ce905d06d7a9366aa7dffe5e6a07c3263
obs-gh-abhinavpappu <141665106+obs-gh-abhinavpappu@users.noreply.github.com>
2025-02-11 16:08:30 -0800

    feat: add schema support for reference tables (#197)

--- observe/resource_reference_table.go
+++ observe/resource_reference_table.go
@@ -9,6 +9,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 
 	observe "github.com/observeinc/terraform-provider-observe/client"
+	gql "github.com/observeinc/terraform-provider-observe/client/meta"
 	"github.com/observeinc/terraform-provider-observe/client/oid"
 	"github.com/observeinc/terraform-provider-observe/client/rest"
 	"github.com/observeinc/terraform-provider-observe/observe/descriptions"
@@ -51,6 +52,24 @@ func resourceReferenceTable() *schema.Resource {
 				Optional:    true,
 				Description: descriptions.Get("reference_table", "schema", "description"),
 			},
+			"schema": {
+				Type:     schema.TypeList,
+				Optional: true,
+				Computed: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"name": {
+							Type:     schema.TypeString,
+							Required: true,
+						},
+						"type": {
+							Type:     schema.TypeString,
+							Required: true,
+						},
+					},
+				},
+				Description: descriptions.Get("reference_table", "schema", "schema"),
+			},
 			"primary_key": {
 				Type:     schema.TypeList,
 				Optional: true,
@@ -74,7 +93,6 @@ func resourceReferenceTable() *schema.Resource {
 				Computed:    true,
 				Description: descriptions.Get("common", "schema", "oid"),
 			},
-			// TODO: support "schema"
 		},
 	}
 }
@@ -85,9 +103,15 @@ func newReferenceTableConfig(data *schema.ResourceData) (input *rest.ReferenceTa
 		return nil, diags
 	}
 
+	schemaInput, diags := newReferenceTableSchemaConfig(data)
+	if diags.HasError() {
+		return nil, diags
+	}
+
 	input = &rest.ReferenceTableInput{
-		Metadata:       *metadataInput,
 		SourceFilePath: data.Get("source_file").(string),
+		Schema:         schemaInput,
+		Metadata:       *metadataInput,
 	}
 
 	return input, diags
@@ -116,24 +140,60 @@ func newReferenceTableMetadataConfig(data *schema.ResourceData, patch bool) (inp
 	return input, diags
 }
 
-func referenceTableToResourceData(d *rest.ReferenceTable, data *schema.ResourceData) (diags diag.Diagnostics) {
-	if err := data.Set("oid", d.Oid().String()); err != nil {
+func newReferenceTableSchemaConfig(data *schema.ResourceData) (input []rest.ReferenceTableSchemaInput, diags diag.Diagnostics) {
+	// We're using GetRawConfig() here instead of Get() or GetOk() because we only want to include
+	// the schema in the request if the user explicitly sets it in the configuration file. Get() and
+	// GetOk() will return whatever's in the state file if there's no value in the config file.
+	// This causes issues because "schema" is also a computed field, so the following fails:
+	//    - User defines a reference table with CSV file containing 1 column (no explicit schema)
+	//        - Schema is computed and set in the state file during a read.
+	//    - User changes the CSV file to contain 2 columns (still no explicit schema)
+	//    - The update fails since we call the API with the new 2-column file and the old 1-column schema.
+	//        - We instead want to provide no schema allowing the API to re-compute it, but
+	//			Get() and GetOk() will return the 1-column schema from the state file.
+	rawConfig := data.GetRawConfig().AsValueMap()
+	if schema, ok := rawConfig["schema"]; ok {
+		for _, v := range schema.AsValueSlice() {
+			s := v.AsValueMap()
+			input = append(input, rest.ReferenceTableSchemaInput{
+				Name: s["name"].AsString(),
+				Type: s["type"].AsString(),
+			})
+		}
+	}
+	return input, diags
+
+}
+
+func referenceTableToResourceData(refTable *rest.ReferenceTable, dataset *gql.Dataset, data *schema.ResourceData) (diags diag.Diagnostics) {
+	if err := data.Set("oid", refTable.Oid().String()); err != nil {
 		diags = append(diags, diag.FromErr(err)...)
 	}
 
-	if err := data.Set("label", d.Label); err != nil {
+	if err := data.Set("label", refTable.Label); err != nil {
 		diags = append(diags, diag.FromErr(err)...)
 	}
 
-	if err := data.Set("description", d.Description); err != nil {
+	if err := data.Set("description", refTable.Description); err != nil {
 		diags = append(diags, diag.FromErr(err)...)
 	}
 
-	if err := data.Set("dataset", oid.DatasetOid(d.DatasetId).String()); err != nil {
+	if err := data.Set("dataset", oid.DatasetOid(refTable.DatasetId).String()); err != nil {
 		diags = append(diags, diag.FromErr(err)...)
 	}
 
-	if err := data.Set("checksum", d.Checksum); err != nil {
+	if err := data.Set("checksum", refTable.Checksum); err != nil {
+		diags = append(diags, diag.FromErr(err)...)
+	}
+
+	schema := make([]map[string]interface{}, len(dataset.Typedef.Def.Fields))
+	for i, field := range dataset.Typedef.Def.Fields {
+		schema[i] = map[string]interface{}{
+			"name": field.Name,
+			"type": field.Type.Rep,
+		}
+	}
+	if err := data.Set("schema", schema); err != nil {
 		diags = append(diags, diag.FromErr(err)...)
 	}
 
@@ -174,7 +234,11 @@ func resourceReferenceTableRead(ctx context.Context, data *schema.ResourceData, 
 		}
 		return diag.Errorf("failed to retrieve reference table: %s", err.Error())
 	}
-	return referenceTableToResourceData(result, data)
+	dataset, err := client.GetDataset(ctx, result.DatasetId)
+	if err != nil {
+		return diag.Errorf("failed to retrieve reference table dataset: %s", err.Error())
+	}
+	return referenceTableToResourceData(result, dataset, data)
 }
 
 func resourceReferenceTableUpdate(ctx context.Context, data *schema.ResourceData, meta interface{}) (diags diag.Diagnostics) {
@@ -187,10 +251,10 @@ func resourceReferenceTableUpdate(ctx context.Context, data *schema.ResourceData
 		return nil
 	}
 
-	// If the file has been modified (i.e. the checksum), need to use the PUT method to fully
-	// replace the reference table. Otherwise, we can use PATCH to only update the metadata.
+	// If the file (i.e. the checksum) or schema have been modified, need to use the PUT method to
+	// fully replace the reference table. Otherwise, we can use PATCH to only update the metadata.
 	// TODO: remove primary_key and label_field below, API will support PATCHing them soon
-	fieldsRequiringPut := []string{"checksum", "primary_key", "label_field"}
+	fieldsRequiringPut := []string{"checksum", "schema", "primary_key", "label_field"}
 	if data.HasChanges(fieldsRequiringPut...) {
 		config, diags := newReferenceTableConfig(data)
 		if diags.HasError() {
